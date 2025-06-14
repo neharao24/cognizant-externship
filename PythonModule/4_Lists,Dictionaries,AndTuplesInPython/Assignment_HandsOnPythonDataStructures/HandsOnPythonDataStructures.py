8c0ef3628ecf7efcd132c415008b437800ac95b9
neharao24 <nehavrao2016@gmail.com>
2025-06-13 22:10:56 -0700

    Python structures assignment coments & project read me

--- PythonModule/4_Lists,Dictionaries,AndTuplesInPython/Assignment_HandsOnPythonDataStructures/HandsOnPythonDataStructures.py
+++ PythonModule/4_Lists,Dictionaries,AndTuplesInPython/Assignment_HandsOnPythonDataStructures/HandsOnPythonDataStructures.py
@@ -5,42 +5,42 @@
 # Task 1 - Working with Lists
 print("\n----------- Task 1 -----------\n")
 
-favFruits = ['strawberry', 'mango', 'peach', 'watermelon', 'blueberry']
+favFruits = ['strawberry', 'mango', 'peach', 'watermelon', 'blueberry']     # List of favourite fruits
 
 
-print("Original list:", favFruits)
+print("Original list:", favFruits)      # Print the original list
 
-favFruits.append('kiwi')
-print("After adding a fruit:", favFruits)
+favFruits.append('kiwi')        # Add a new fruit to the list
+print("After adding a fruit:", favFruits)       # Print the updated list
 
-favFruits.remove('blueberry')
-print("After removing a fruit:", favFruits)
+favFruits.remove('blueberry')       # Remove a fruit from the list
+print("After removing a fruit:", favFruits)     # Print the updated list
 
-print("Reversed List:", favFruits[::-1])
+print("Reversed List:", favFruits[::-1])        # Print the reversed list
 
 
 
 # Task 2 - Exploring Dictionaries
 print("\n\n----------- Task 2 -----------\n")
 
-info = {"name": "Neha", "age": "21", "city": "Tracy"}
+info = {"name": "Neha", "age": "21", "city": "Tracy"}       # Dictionary with personal info
 
-info["favorite color"] = "Pink"
-info["city"] = "Tempe"
+info["favorite color"] = "Pink"     # Add a new key-value pair
+info["city"] = "Tempe"      # Update an existing key-value pair
 
 
-keys = list(info.keys())
-print("Keys: ", end="")
-for i in range(len(keys)):
-    if  i == len(keys) - 1:
+keys = list(info.keys())        # Get the keys of the dictionary
+print("Keys: ", end="")     # Print the keys
+for i in range(len(keys)):      # Loop through the keys, print each & add comma
+    if  i == len(keys) - 1:    
         print(keys[i])
-    else:
-        print(f"{keys[i]}, ", end="")
+    else:     
+        print(f"{keys[i]}, ", end="")  
 
 
-values = list(info.values())
-print("Values: ", end="")
-for i in range(len(values)):
+values = list(info.values())        # Get the values of the dictionary
+print("Values: ", end="")       # Print the values
+for i in range(len(values)):        # Loop through the values, print each & add comma
     if  i == len(values) - 1:
         print(values[i])
     else:
@@ -53,12 +53,12 @@ for i in range(len(values)):
 # Task 3 - Using Tuples
 print("\n\n----------- Task 3 -----------\n")
 
-favThings = ('Big Hero 6', 'Hello', 'Ruby Red')
+favThings = ('Big Hero 6', 'Hello', 'Ruby Red')     # Tuple of favourite things
 
-try:
+try:        # Try to modify the tuple
     favThings[0] = 'Spiderman'
-except TypeError:
+except TypeError:       # If it's a tuple, it will throw a TypeError
     print("Oops! Tuples cannot be changed.")
 
-print(f"Length of tuple: {len(favThings)}\n")
+print(f"Length of tuple: {len(favThings)}\n")       # Print the length of the tuple
 
---
+++ PythonModule/4_Lists,Dictionaries,AndTuplesInPython/Project_ImplementYourOwnDataStructures/README.md
@@ -0,0 +1,3 @@
+# Project_ImplementYourOwnDataStructure
+
+This project is a Python program that simulates an inventory management system using a combination of dictionaries and tuples to store item data, also includes interactive features for managing stock.
