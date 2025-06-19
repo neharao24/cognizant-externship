0c2716a00aecc758eb673b8dc1758155a965cf63
neharao24 <nehavrao2016@gmail.com>
2025-06-19 00:40:08 -0700

    Added Capstone project & README

---
+++ 1_PythonModule/7_CapstoneProject/python-capstone-finance-tracker/README.md
@@ -0,0 +1,21 @@
+# 7_CapstoneProject
+
+Personal Finance Tracker:
+    This project is a simple command-line program that helps you keep track of your expenses by category. You can add expenses with a description, category, and amount, view all your expenses, and see a summary of how much you’ve spent in each category. This Personal Finance Tracker program uses a menu and checks for invalid input so it’s easy to use and user-friendly.
+
+
+How to Run:
+    1. Make sure Python 3 is installed.
+    2. Download or clone this repository.
+    4. Press 'Run' in your IDE OR Open a terminal and navigate to the project folder.
+        a. If running in terminal, use: 'python finance_tracker.py'
+    5. Follow the menu to add expenses, view them, or see your spending summary.
+
+
+Python Concepts Used:
+    - Functions
+    - Lists, Dictionaries, & Tuples
+    - Variables & Data types
+    - User input, Strings, & String operations
+    - If/Else statements, While loops, & For loops
+    - Exception handling (try/except)
--- 1_PythonModule/7_CapstoneProject/python-capstone-finance-tracker/finance_tracker.py
+++ 1_PythonModule/7_CapstoneProject/python-capstone-finance-tracker/finance_tracker.py
@@ -2,120 +2,95 @@
 # Neha Rao
 
 
-inventory = {}      # empty dictionary for inventory
+expenses = {}      # empty dictionary for expenses
+
+
+def add_expense(expenses):      # Function to add expenses
+    try:
+        # Get the expense description from user
+        description = input("Enter expense description: ")
+        if not description:
+            print("Description cannot be empty.")
+            return
+        
+        # Get expense category from user
+        category = input("Enter category: ")
+        if not category:
+            print("Category cannot be empty.")
+            return
+        
+        # Get expense amount from user
+        amount_input = input("Enter amount: ")
+        amount = float(amount_input)
+        if amount < 0:
+            print("Amount cannot be negative.")
+            return
+        
+        # Add expense to dictionary, creates new category if doesn't already exist
+        expenses.setdefault(category, []).append((description, amount))
+        print("Expense added successfully.")
+
+    except ValueError:
+        print("Invalid amount. Please enter a number.")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+
+
+
+def view_expenses(expenses):        # Function to view expenses
+    if expenses:        # Check if expenses dictionary is not empty
+        for category, items in expenses.items():        # Loop through each category
+            print(f"Category: {category}")
+            for description, amount in items:       # Loop through each expense in category
+                print(f"  - {description}: ${amount:.2f}")
+
+    else:       # If expenses dictionary is empty
+        print("No expenses recorded yet.")
+        return
+
+
+
+
+def view_summary(expenses):     # Function to view summary
+    if expenses:        # Check if expenses dictionary is not empty
+        print("Summary:")
+
+        for category, items in expenses.items():        # Loop through each category
+            total = 0
+            for amount in items:        # Loop through each expense in category and sum up the amounts
+                total += amount[1]
+            print(f"{category}: ${total:.2f}")
+
+    else:       # If expenses dictionary is empty
+        print("No expenses recorded yet.")
+        return
+
+
+
 
 # Loop until user chooses to exit
-print("\nWelcome to the Inventory Manager!")
+print("\nWelcome to the Personal Finance Tracker!")
 while True:
-    print("Select an option:")
-    print("1. Add item to inventory")
-    print("2. Remove item from inventory")
-    print("3. Update item quantity")
-    print("4. Update item price")
-    print("5. Display inventory")
-    print("6. Calculate Total Value of Inventory")
-    print("7. Exit")
-    userChoice = input("Enter your choice: ")
+    print("What would you like to do?")
+    print("1. Add Expense")
+    print("2. View All Expenses")
+    print("3. View Summary")
+    print("4. Exit")
+    userChoice = input("Choose an option: ")
     print("\n")
 
-    if userChoice == "1":       # Add item to inventory
-        # Get item name, quantity, price from user
-        itemName = input("Enter item name: ")
-        itemQuantity = int(input("Enter item quantity: "))
-        itemPrice = float(input("Enter item price: $"))
+    if userChoice == "1":       # Add Expense
+        add_expense(expenses)
 
-        # Add item to inventory dictionary as tuple
-        inventory[itemName] = (itemQuantity, itemPrice)
-        print(f"\nItem {itemName} added to inventory.")
+    elif userChoice == "2":     # View All Expenses
+       view_expenses(expenses)
 
-        # Updated inventory
-        print(" Updated inventory:")
-        for item, details in inventory.items():
-            print(f"  -Item: {item}, Quantity: {details[0]}, Price: ${details[1]}")
+    elif userChoice == "3":     # View Summary
+        view_summary(expenses)
 
-
-
-    elif userChoice == "2":     # Remove item from inventory
-        # Get item name from user
-        itemName = input("Enter item name: ")
-
-        if itemName in inventory:       # Check if item exists in inventory
-            # Remove item from inventory dictionary
-            del inventory[itemName]
-            print(f"\nItem '{itemName}' removed from inventory.")
-
-            # Updated inventory
-            print(" Updated inventory:")
-            for item, details in inventory.items():
-                print(f"  -Item: {item}, Quantity: {details[0]}, Price: ${details[1]}")
-
-        else:       # If item does not exist in inventory
-            print(f"\nItem '{itemName}' not found in inventory.")
-
-
-
-    elif userChoice == "3":     # Update item quantity
-        # Get item name and new quantity from user
-        itemName = input("Enter item name: ")
-
-        if itemName in inventory:       # Check if item exists in inventory
-            # Get new quantity from user & update inventory
-            newQuantity = int(input("Enter new quantity: "))
-            inventory[itemName] = (newQuantity, inventory[itemName][1])
-            print(f"\nItem '{itemName}' quantity updated.")
-
-            # Updated inventory
-            print(" Updated inventory:")
-            for item, details in inventory.items():
-                print(f"  -Item: {item}, Quantity: {details[0]}, Price: ${details[1]}")
-
-        else:       # If item does not exist in inventory
-            print(f"\nItem '{itemName}' not found in inventory.")
-
-
- 
-    elif userChoice == "4":     # Update item price
-        # Get item name and new price from user
-        itemName = input("Enter item name: ")
-
-        if itemName in inventory:       # Check if item exists in inventory
-            # Get new price from user & update inventory
-            newPrice = float(input("Enter new price: $"))
-            inventory[itemName] = (inventory[itemName][0], newPrice)
-            print(f"\nItem '{itemName}' price updated.")
-
-            # Updated inventory
-            print(" Updated inventory:")
-            for item, details in inventory.items():
-                print(f"  -Item: {item}, Quantity: {details[0]}, Price: ${details[1]}")
-
-        else:       # If item does not exist in inventory
-            print(f"\nItem '{itemName}' not found in inventory.")
-
-
-
-    elif userChoice == "5":     # Display inventory
-        if not inventory:       # Check if inventory is empty
-            print("Inventory is empty.")
-        else:       # If inventory is not empty, display it
-            print("Current Inventory:")
-            for item, details in inventory.items():
-                print(f" -Item: {item}, Quantity: {details[0]}, Price: ${details[1]}")
-
-
-
-    elif userChoice == "6":     # Calculate Total Value
-        totalValue = 0
-        for item, details in inventory.items():     # Iterate through inventory & calculate total value
-            totalValue += details[0] * details[1]
-        print(f"Total value of inventory: ${totalValue:.2f}")      
-
-
-
-    elif userChoice == "7":     # Exit
+    elif userChoice == "4":     # Exit
         print("Exiting program...")
         break
-
 
     else:       # Invalid choice
         print("\nInvalid choice. Please try again.")
@@ -124,5 +99,5 @@ while True:
     print("\n\n")
 
 
-print("Thank you for using the Inventory Manager! 👋")
+print("Thank you for using the Personal Finance Tracker!\nGoodbye! 👋")
 
